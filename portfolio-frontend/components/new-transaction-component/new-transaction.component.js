a9167101d65df7fbb192c4c22a9d55d47fcc3fee
@aaronparker2319 <aaronparker2319@gmail.com>
2021-07-29 19:28:40 -0400

    pushing for meeting

--- portfolio-frontend/components/new-transaction-component/new-transaction.component.js
+++ portfolio-frontend/components/new-transaction-component/new-transaction.component.js
@@ -1,6 +1,29 @@
+fetch("tickers.json").then(response => {
+    return response.json();
+}).then(data => {
+    const tickerList = data;
+    
+    var tick = document.getElementById("ticker");
+    
+    console.log(Object.entries(tickerList).values(tickerList))
+
+    tick.addEventListener('change', function(){
+        console.log(tick.value);
+        // for (const t in Object.values(tickerList)){
+        //     console.log(t.includes({'ACT Symbol':tick.value}));
+        // }
+        // if(Object.values(tickerList).includes({'ACT Symbol':tick.value})) {
+        //     console.log("yay");
+        //     return true;
+        // }
+    })
+});
+
 
 document.getElementById("new-txn-form").addEventListener("submit", function(t){
     t.preventDefault();
+
+    
 
     var tickerError = document.getElementById("ticker-error");
     tickerError.hidden = true;
@@ -9,6 +32,8 @@ document.getElementById("new-txn-form").addEventListener("submit", function(t){
     var shareAmount = document.getElementById("share-amount").value;
     var sharePrice = document.getElementById("share-price").value;
     var note = document.getElementById("note").value;
+
+    
 
     // var isValidTicker = validateTicker(tickerSymobol);
 
